135e46cbf756c2e9e6e3f913919425a753c16eaa
Sinuhe135 <sinuhe9999@gmail.com>
2024-06-22 01:58:41 -0700

    Logica comando apostar

--- PruebaNodeJS/scr/index.js
+++ PruebaNodeJS/scr/index.js
@@ -19,22 +19,74 @@ client.on('messageCreate',(message) => {
         return;
     }
 
-    let mensaje = message.content;
-        
-    if(!mensaje.startsWith("csn"))
-    {
-        return;
-    }
-
-    mensaje = mensaje.substring(3).trim();
-
-    if(mensaje==="pp")
-    {
-        message.reply("pene");
-        return;
-    }
-
-    message.reply("Comando no reconocido");
+    leerMensaje(message);
 });
+
+function leerMensaje(message)
+{
+    let palabras = message.content.split(/\s+/);
+
+    if(palabras[0] != "csn")
+    {
+        return;
+    }
+
+    if(typeof palabras[1] === "undefined")
+    {
+        message.reply("Lista de comandos:\nbet\nap");
+    }
+    else if(palabras[1]==="apostar")
+    {
+        funcionBet(palabras.slice(2), message);
+    }
+    else if(palabras[1]==="fichas")
+    {
+        funcionFichas(message);
+    }
+    else if(palabras[1]==="pp")
+    {
+        funcionPp(message);
+    }
+    else
+    {
+        message.reply("Comando no reconocido");
+    }
+
+}
+
+function funcionBet(palabrasBet, message)
+{
+    if(typeof palabrasBet[0] ===  "undefined")
+    {
+        message.reply("Se debe apostar un numero entero positivo");
+        return;
+    
+    }
+    if(typeof palabrasBet[1] !==  "undefined")
+    {
+        message.reply("Se debe apostar un numero entero positivo");
+        return;
+    }
+
+    if(palabrasBet[0].search(/^\d+$/) == -1)
+    {
+        message.reply("Se debe apostar un numero entero positivo");
+        return;
+    }
+
+    fichas = parseInt(palabrasBet[0]);
+
+    message.reply("Has apostado " + fichas);
+}
+
+function funcionPp(message)
+{
+    message.reply("polla");
+}
+
+function funcionPp(message)
+{
+    message.reply("polla");
+}
 
 client.login("MTIzMjIzODk0NDExNzE5ODkxOA.GI_qHj.COzNx7TqC8r-u9TfhlAbgOLYmhRnw7AK-s2Vmg");
